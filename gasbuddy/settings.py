bf1b7de46111ff8f63ec0113ab85b496789ec5af
inspectorbean <erkoak@gmail.com>
2017-08-20 14:08:52 -0400

    settings

--- gasbuddy/settings.py
+++ gasbuddy/settings.py
@@ -79,12 +79,7 @@ WSGI_APPLICATION = 'gasbuddy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
@@ -145,3 +140,11 @@ if acc_keys.prod==True:
     DEBUG = False
 
     ALLOWED_HOSTS = ['www.eve-gasbuddy.com']
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
