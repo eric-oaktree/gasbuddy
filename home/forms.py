ee9cb6f556ffd6bdbf03ac5cbec5b3f03946328e
inspectorbean <erkoak@gmail.com>
2017-08-20 15:39:50 -0400

    final fix?

--- home/forms.py
+++ home/forms.py
@@ -3,11 +3,17 @@ from django import forms
 from .models import Gas, Region, Station, Site, Ship, Harvester, Setup
 
 class GasForm(forms.Form):
-    ship = forms.ModelChoiceField(queryset=Ship.objects.all(), initial=Ship.objects.get(name='Prospect'), label='Ship: ')
-    harvester = forms.ModelChoiceField(queryset=Harvester.objects.all(), initial=Harvester.objects.get(harv_id='25812'), label='Harvester: ')
-    skill = forms.IntegerField(initial=5, label='Mining Frigates Skill: ')
+    try:
+        ship = forms.ModelChoiceField(queryset=Ship.objects.all(), initial=Ship.objects.get(name='Prospect'), label='Ship: ')
+        harvester = forms.ModelChoiceField(queryset=Harvester.objects.all(), initial=Harvester.objects.get(harv_id='25812'), label='Harvester: ')
+        skill = forms.IntegerField(initial=5, label='Mining Frigates Skill: ')
+    except:
+        pass
 
 class SiteForm(forms.Form):
-    ship = forms.ModelChoiceField(queryset=Ship.objects.all(), initial=Ship.objects.get(name='Prospect'), label='Ship: ')
-    harvester = forms.ModelChoiceField(queryset=Harvester.objects.all(), initial=Harvester.objects.get(harv_id='25812'), label='Harvester: ')
-    skill = forms.IntegerField(initial=5, label='Mining Frigates Skill: ')
+    try:
+        ship = forms.ModelChoiceField(queryset=Ship.objects.all(), initial=Ship.objects.get(name='Prospect'), label='Ship: ')
+        harvester = forms.ModelChoiceField(queryset=Harvester.objects.all(), initial=Harvester.objects.get(harv_id='25812'), label='Harvester: ')
+        skill = forms.IntegerField(initial=5, label='Mining Frigates Skill: ')
+    except:
+        pass
