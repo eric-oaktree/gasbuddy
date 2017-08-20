bf1345d7191dfa060f6625a3172a79651b727dbd
inspectorbean <erkoak@gmail.com>
2017-08-20 15:34:51 -0400

    fix

--- home/forms.py
+++ home/forms.py
@@ -3,17 +3,11 @@ from django import forms
 from .models import Gas, Region, Station, Site, Ship, Harvester, Setup
 
 class GasForm(forms.Form):
-    '''
     ship = forms.ModelChoiceField(queryset=Ship.objects.all(), initial=Ship.objects.get(name='Prospect'), label='Ship: ')
     harvester = forms.ModelChoiceField(queryset=Harvester.objects.all(), initial=Harvester.objects.get(harv_id='25812'), label='Harvester: ')
     skill = forms.IntegerField(initial=5, label='Mining Frigates Skill: ')
-    '''
-    pass
 
 class SiteForm(forms.Form):
-    '''
     ship = forms.ModelChoiceField(queryset=Ship.objects.all(), initial=Ship.objects.get(name='Prospect'), label='Ship: ')
     harvester = forms.ModelChoiceField(queryset=Harvester.objects.all(), initial=Harvester.objects.get(harv_id='25812'), label='Harvester: ')
     skill = forms.IntegerField(initial=5, label='Mining Frigates Skill: ')
-'''
-    pass
